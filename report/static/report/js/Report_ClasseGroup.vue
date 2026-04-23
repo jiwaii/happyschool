6f4c0eb1d922e9d9844dd1b3090e2316f839ce06
jiwaii <lj.laurent.jean.yves@gmail.com>
2026-04-23 09:55:19 +0200

    REPORT: add ClasseGroup pages

---
+++ report/static/report/js/Report_ClasseGroup.vue
@@ -0,0 +1,60 @@
+<template>
+<h1>Classes par année d'étude</h1>
+    <div>
+    <!-- <BTable
+      striped
+      hover
+      :items="this.entries"
+      :fields="this.fields"
+    /> -->
+    </div>
+    <b-row
+    class="card px-4 mt-2" 
+    v-for="classeGroup in entries"
+    >
+        <div>
+            <h4>{{ classeGroup.title }} :</h4>
+            <ClassePad
+            v-for="classe in classeGroup.classes"
+            :label="classe"/>
+        </div>
+    </b-row>
+</template>
+
+<script>
+import axios from 'axios';
+import Moment from "moment";
+import "moment/dist/locale/fr"
+import ClassePad from './Report_ClassePad.vue';
+import { str } from 'ajv';
+Moment.locale("fr")
+
+export default{
+    components:{
+        ClassePad
+    },
+    data: function(){
+        return {
+            entries : [],
+            fields : [
+                {key: 'title', label:'Titre'},
+                {key: 'studyYear', label:'Année d\'étude'},
+            ]
+        }
+    },
+    methods:{
+        loadEntries:function(){
+            return axios.get("api/classegroup/").then(
+                response => {
+                    this.entries = response.data.results;
+                }
+            )
+        }
+    },
+    mounted:function() {
+        this.loadEntries();
+
+    }
+}
+
+</script>
---
+++ report/static/report/js/Report_ClassePad.vue
@@ -0,0 +1,24 @@
+<script>
+export default{
+	props:['label']
+}
+</script>
+
+<template>
+	<div style="
+			border-style:solid;
+			font-weight: bold;
+			background-image: linear-gradient(135deg,#579fd1,#eaf3fc);
+			padding: 5px;
+			margin: 5px;
+			display: inline-block;
+			border-width: 2px;
+			border-radius: 5px;
+			border-color: #579fd1;
+			width: 40px;
+			text-align: center;
+		"
+	>
+		{{	label }}
+	</div>
+</template>
